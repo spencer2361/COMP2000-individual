0d6ca81d2038b796b77fd34b7929a14af51d95dc
MarcussShidiakk <marcus.shidiak@students.mq.edu.au>
2026-08-18 10:45:34 +1000

    Marcus - Initial Commit
    Added some example code too.

---
+++ Sandbox.java
@@ -0,0 +1,9 @@
+public class Sandbox {
+public static void main(String[] args) {
+    System.out.println("hgcjkbhgv!");
+
+    Frame a = new Frame();
+    System.out.println(a.testFunc());
+    }
+}
+
