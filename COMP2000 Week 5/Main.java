0a16f10213300fa9e1f5927076fc21ef8dbb2f08
MarcussShidiakk <marcus.shidiak@students.mq.edu.au>
2026-08-25 10:14:54 +1000

    Fixed Mistake
    Put my work in the wrong folder oops :/

--- COMP2000 Week 5/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Container<?> strings = new Container<>();
-        Container rawContainer = strings;
-        rawContainer.add(1);
-        System.out.println(rawContainer);
-    }
-}
-
